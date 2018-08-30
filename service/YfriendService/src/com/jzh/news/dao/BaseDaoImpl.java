43e1598248a3d4f35ac6a356396c5eedb818c07a
Jaylon <113531420@qq.com>
2018-08-30 13:35:14 +0800

    commit the latest changes

--- service/YfriendService/src/com/jzh/news/dao/BaseDaoImpl.java
+++ service/YfriendService/src/com/jzh/news/dao/BaseDaoImpl.java
@@ -1,15 +1,18 @@
 package com.jzh.news.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class BaseDaoImpl {
 
 	/**
-	 *  Õ∑≈◊ ‘¥
+	 * ÈáäÊîæËµÑÊ∫ê
 	 * 
 	 * @param rs
 	 * @param pstmt
@@ -32,38 +35,56 @@ public class BaseDaoImpl {
 		}
 	}
 
-	public Connection getConnection() {
+	private static String url = null;
+	 
+	private static String openFireUrl = null; 
+	private static String user  = null;  
+	private static String password = null; 
+	private static String driverClass = null;
+	private static Properties prop = new Properties();
+	
+	static {
+		 Class clazz = BaseDaoImpl.class;
+		 InputStream in = clazz.getResourceAsStream("/jdbc.properties");  
+	          try {
+				prop.load(in);
+			} catch (IOException e) { 
+				e.printStackTrace();
+			}
+	      url = prop.getProperty("jdbc.mysql.url");
+	      openFireUrl = prop.getProperty("jdbc.mysql.openFireUrl");
+          user = prop.getProperty("jdbc.mysql.username");
+          password = prop.getProperty("jdbc.mysql.password");
+          driverClass = prop.getProperty("jdbc.mysql.driverClassName");
+          System.out.println("url: "+url+" user: "+user+" password: "+password+" driverClass: "+driverClass);  
+
+	}
+	
+	public Connection getConnection()   {
 		Connection conn = null;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/news?characterEncoding=UTF-8&autoReconnect=true", "root", "root");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			Class.forName(driverClass);
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) { 
 			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) { 
 			e.printStackTrace();
-		}
-
+		}  
 		return conn;
 
 	}
 
 	public Connection getOpenfireConnection() {
 		Connection conn = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
+		try { 
+			Class.forName("driverClass");
 			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/openfire", "root", "root");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+					 this.openFireUrl, "root", "root");
+		} catch (ClassNotFoundException e) { 
 			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) { 	
 			e.printStackTrace();
 		}
-
 		return conn;
 
 	}
