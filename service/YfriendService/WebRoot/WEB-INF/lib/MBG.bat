708d6fb5df17e1fae3d314f7909f0397ab0d4ca6
Jaylon <113531420@qq.com>
2018-09-22 10:24:04 +0800

    commit all

--- service/YfriendService/WebRoot/WEB-INF/lib/MBG.bat
+++ service/YfriendService/WebRoot/WEB-INF/lib/MBG.bat
@@ -1,2 +1,4 @@
+cd D:\workspace\mobile\xmpp-master\service\YfriendService\WebRoot\WEB-INF\lib\
 java -jar mybatis-generator-core-1.3.7.jar -configfile generatorConfig.xml -overwrite
+explorer D:\workspace\mobile\xmpp-master\service\YfriendService\WebRoot\WEB-INF\lib\com\jzh\news\entity
 pause
